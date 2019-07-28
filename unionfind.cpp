d8202ee277d8548317d3e7690267295d710207b4
matthew7319 <41511768+matthew7319@users.noreply.github.com>
2019-07-28 13:25:09 +0800

    Update unionfind.cpp
    compact ufds with path compression and union by size

--- unionfind.cpp
+++ unionfind.cpp
@@ -1,84 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef pair< int, int> ii;         // parent, rank
-ii arr[1000000];                    // index represents the value
+vector<int> parent;
+vector<int> sz;
 
-int find_parent( int n){
-    if(arr[n].first==-1)
-        return n;
-    vector<int>v;
-    while( arr[n].first != -1){
-        v.push_back(n);
-        n = arr[n].first;
-    }
-    for(auto x:v)
-        arr[x].first=n;   // optimization
-    return n;
+int find(int a) {
+	if (parent[a] == -1) return a;
+	return parent[a] = find(parent[a]);
 }
 
-void union_join( int n1, int n2){
-    int start1 = n1;
-    int start2 = n2;
-    int super_parent;
-    n1 = find_parent(n1);
-    n2 = find_parent(n2);
-
-    // same parent so do nothing
-    if( n1 == n2){
-        //do nothing
-    }
-    // n2 parent is the new super-parent
-    else if( arr[n2].second > arr[n1].second ){
-        arr[n1].first = n2;
-        super_parent = n2;
-
-    }
-    // n1 parent is the new super-parent
-    else if( arr[n1].second > arr[n2].second ){
-        arr[n2].first = n1;
-        super_parent = n1;
-    }
-    // n1 parent is the new super-parent + rank ++;
-    else{
-        arr[n2].first = n1;
-        super_parent = n1;
-        arr[n1].second++;
-    }
-    if( arr[n1].first != -1)
-        n2 = super_parent;
-    if( arr[n2].first != -1)
-        n2 = super_parent;    
+void join(int a, int b) {
+	a = find(a);
+	b = find(b);
+	if (a == b) return;
+	sz[b] += sz[a];
+	parent[a] = b;
 }
 
-void reset(){
-    for(int i=0; i<1000000; i++){
-        arr[i] = ii(-1, 0);
-    }
+void init(int N) {
+	parent.assign(N, -1);
+	sz.assign(N, 1);
 }
 
-int main(){
-    // freopen("input.txt", "r", stdin);
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
-    reset();
-    int i, n, q;
-    scanf("%d %d", &n, &q);
-
-    char ch;
-    int n1, n2;
-
-    for( i=0; i<q; i++){
-        scanf("%c", &ch);
-
-        scanf("%c %d %d", &ch, &n1, &n2);
-        if( ch == '='){
-            union_join(n1, n2);
-        }
-        else{
-            if( find_parent(n1) == find_parent(n2))
-                printf("yes\n");
-            else
-                printf("no\n");
-        }
-    }
+int main() {
+	// freopen("input.txt", "r", stdin);
+	int N, Q, u, v;
+	char ch;
+	scanf("%d %d ", &N, &Q);
+	init(N);
+	for (int i = 0; i < Q; i++) {
+		scanf("%c %d %d ", &ch, &u, &v);
+		if (ch == '?') {
+			printf((find(u) == find(v) ? "yes\n" : "no\n"));
+		}
+		else {
+			join(u, v);
+		}
+	}
 }
